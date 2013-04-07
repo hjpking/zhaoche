f3077cfa012eff1a0b7c0b818f053e84affb7375
evanhou <hjpking@gmail.com>
2013-04-07 22:45:34 +0800

    modify

--- application/models/model_order.php
+++ application/models/model_order.php
@@ -143,7 +143,7 @@ class model_order extends MY_Model
             'chauffeur_id' => '',
             'chauffeur_login_name' => '',
             'chauffeur_phone' => '',
-            'status' => '0',
+            'status' => '2',
         );
 
         $this->db->where('order_sn', $orderSn);
