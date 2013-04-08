57ee19e31613e00a9f2e70046d1404f94020f374
evanhou <hjpking@gmail.com>
2013-04-08 15:32:28 +0800

    test

--- application/models/model_pay_alipay.php
+++ application/models/model_pay_alipay.php
@@ -28,7 +28,7 @@ class model_pay_alipay extends MY_Model
         //$string = "<result><is_success>T</is_success><content>" . $str . "</content><sign>" . $sign . "</sign></result>";
         $string = array(
             'content' => urlencode($str),
-            'sign' => $sign,
+            'sign' => urlencode($sign),
         );
         //return $data['order_sn'];
         return $string;
