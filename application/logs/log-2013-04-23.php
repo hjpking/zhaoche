ee3ae0d28792320ecd08cdc5069eae1ecced3a53
evanhou <hjpking@gmail.com>
2013-04-24 14:36:55 +0800

    test

---
+++ application/logs/log-2013-04-23.php
@@ -0,0 +1,46 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed'); ?>
+
+ERROR - 2013-04-23 09:38:20 --> 404 Page Not Found --> favicon.ico
+ERROR - 2013-04-23 09:38:20 --> 404 Page Not Found --> favicon.ico
+ERROR - 2013-04-23 11:39:28 --> 404 Page Not Found --> favicon.ico
+ERROR - 2013-04-23 11:39:34 --> 404 Page Not Found --> favicon.ico
+ERROR - 2013-04-23 15:55:23 --> Severity: Notice  --> Undefined property: CI_Loader::$amInfo /data/www/zhaoche/application/views/navbar.php 12
+ERROR - 2013-04-23 15:55:23 --> Severity: Notice  --> Undefined property: CI_Loader::$cc /data/www/zhaoche/application/views/index.php 11
+ERROR - 2013-04-23 15:56:50 --> Severity: Notice  --> Undefined property: CI_Loader::$amInfo /data/www/zhaoche/application/views/navbar.php 12
+ERROR - 2013-04-23 15:56:50 --> Severity: Notice  --> Undefined property: CI_Loader::$cc /data/www/zhaoche/application/views/index.php 11
+ERROR - 2013-04-23 15:57:09 --> Severity: Notice  --> Undefined property: CI_Loader::$amInfo /data/www/zhaoche/application/views/navbar.php 12
+ERROR - 2013-04-23 15:57:09 --> Severity: Notice  --> Undefined property: CI_Loader::$cc /data/www/zhaoche/application/views/index.php 11
+ERROR - 2013-04-23 16:04:51 --> 404 Page Not Found --> other/versionCheck
+ERROR - 2013-04-23 16:04:51 --> 404 Page Not Found --> favicon.ico
+ERROR - 2013-04-23 16:05:12 --> 404 Page Not Found --> other/versionCheck
+ERROR - 2013-04-23 16:06:46 --> Severity: Notice  --> Undefined property: CI_Loader::$amInfo /data/www/zhaoche/application/views/navbar.php 12
+ERROR - 2013-04-23 16:06:46 --> Severity: Notice  --> Undefined property: CI_Loader::$cc /data/www/zhaoche/application/views/index.php 11
+ERROR - 2013-04-23 16:07:35 --> Severity: Notice  --> Undefined property: CI_Loader::$amInfo /data/www/zhaoche/application/views/navbar.php 12
+ERROR - 2013-04-23 16:07:35 --> Severity: Notice  --> Undefined property: CI_Loader::$cc /data/www/zhaoche/application/views/index.php 11
+ERROR - 2013-04-23 16:11:43 --> 404 Page Not Found --> favicon.ico
+ERROR - 2013-04-23 16:11:44 --> 404 Page Not Found --> favicon.ico
+ERROR - 2013-04-23 16:11:50 --> 404 Page Not Found --> favicon.ico
+ERROR - 2013-04-23 16:12:28 --> 404 Page Not Found --> favicon.ico
+ERROR - 2013-04-23 16:12:33 --> 404 Page Not Found --> favicon.ico
+ERROR - 2013-04-23 16:12:52 --> 404 Page Not Found --> favicon.ico
+ERROR - 2013-04-23 16:12:55 --> 404 Page Not Found --> favicon.ico
+ERROR - 2013-04-23 16:13:07 --> 404 Page Not Found --> favicon.ico
+ERROR - 2013-04-23 16:15:26 --> 404 Page Not Found --> favicon.ico
+ERROR - 2013-04-23 16:15:27 --> 404 Page Not Found --> favicon.ico
+ERROR - 2013-04-23 21:49:40 --> 404 Page Not Found --> favicon.ico
+ERROR - 2013-04-23 21:53:10 --> Severity: Notice  --> A non well formed numeric value encountered /data/www/zhaoche/system/core/Common.php 658
+ERROR - 2013-04-23 21:53:10 --> Severity: Notice  --> A non well formed numeric value encountered /data/www/zhaoche/system/core/Common.php 658
+ERROR - 2013-04-23 21:53:10 --> Severity: Notice  --> A non well formed numeric value encountered /data/www/zhaoche/system/core/Common.php 658
+ERROR - 2013-04-23 21:53:10 --> Severity: Notice  --> A non well formed numeric value encountered /data/www/zhaoche/system/core/Common.php 658
+ERROR - 2013-04-23 21:53:10 --> Severity: Notice  --> A non well formed numeric value encountered /data/www/zhaoche/system/core/Common.php 658
+ERROR - 2013-04-23 21:53:10 --> Severity: Notice  --> A non well formed numeric value encountered /data/www/zhaoche/system/core/Common.php 658
+ERROR - 2013-04-23 21:53:10 --> Severity: Notice  --> A non well formed numeric value encountered /data/www/zhaoche/system/core/Common.php 658
+ERROR - 2013-04-23 21:53:10 --> Severity: Notice  --> A non well formed numeric value encountered /data/www/zhaoche/system/core/Common.php 658
+ERROR - 2013-04-23 21:53:10 --> Severity: Notice  --> A non well formed numeric value encountered /data/www/zhaoche/system/core/Common.php 658
+ERROR - 2013-04-23 21:53:10 --> Severity: Notice  --> A non well formed numeric value encountered /data/www/zhaoche/system/core/Common.php 658
+ERROR - 2013-04-23 21:53:10 --> Severity: Notice  --> A non well formed numeric value encountered /data/www/zhaoche/system/core/Common.php 658
+ERROR - 2013-04-23 21:53:10 --> Severity: Notice  --> A non well formed numeric value encountered /data/www/zhaoche/system/core/Common.php 658
+ERROR - 2013-04-23 21:53:10 --> Severity: Notice  --> A non well formed numeric value encountered /data/www/zhaoche/system/core/Common.php 658
+ERROR - 2013-04-23 21:53:10 --> Severity: Notice  --> A non well formed numeric value encountered /data/www/zhaoche/system/core/Common.php 658
+ERROR - 2013-04-23 21:53:10 --> Severity: Notice  --> A non well formed numeric value encountered /data/www/zhaoche/system/core/Common.php 658
+ERROR - 2013-04-23 22:39:16 --> 404 Page Not Found --> favicon.ico
