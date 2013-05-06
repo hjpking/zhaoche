b27ca304d38dac2e7197e2e4469db1de01b0198a
root <root@localhost.localdomain>
2013-05-06 22:16:21 +0800

    test

--- application/logs/log-2013-05-01.php
+++
@@ -1,6 +0,0 @@
-<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed'); ?>
-
-ERROR - 2013-05-01 20:32:36 --> 404 Page Not Found --> favicon.ico
-ERROR - 2013-05-01 21:23:17 --> Severity: Warning  --> Division by zero /data/www/zhaoche/application/controllers/api/order.php 696
-ERROR - 2013-05-01 21:35:57 --> 404 Page Not Found --> favicon.ico
-ERROR - 2013-05-01 21:35:57 --> 404 Page Not Found --> favicon.ico
